0a55a159291fda933d615beb00516d8418e2d978
Moises Velasquez <moises007@gmail.com>
2026-02-11 10:43:31 -0500

    feat(service): Implement ticket service

--- src/presentation/services/ticket.service.ts
+++ src/presentation/services/ticket.service.ts
@@ -2,7 +2,7 @@ import { UuidAdapter } from "../../config/uuid.adapter";
 import { Ticket } from "../../domain/interfaces/ticket.interface";
 
 export class TicketService {
-  private readonly _tickets: Ticket[] = [
+  public tickets: Ticket[] = [
     { id: UuidAdapter.v7(), number: 1, createdAt: new Date(), done: false },
     { id: UuidAdapter.v7(), number: 2, createdAt: new Date(), done: false },
     { id: UuidAdapter.v7(), number: 3, createdAt: new Date(), done: false },
@@ -11,5 +11,59 @@ export class TicketService {
     { id: UuidAdapter.v7(), number: 6, createdAt: new Date(), done: false },
   ];
 
+  private readonly workingOnTickets: Ticket[] = [];
+
   constructor() {}
+
+  public get pendingTickets(): Ticket[] {
+    return this.tickets.filter((ticket) => !ticket.handleAtDesk);
+  }
+
+  public get lastWorkingOnTickets(): Ticket[] {
+    return this.workingOnTickets.splice(0, 4);
+  }
+
+  public get lastTicketNumber(): number {
+    return this.tickets.at(-1)?.number ?? 0;
+  }
+
+  public createTicket() {
+    const ticket: Ticket = {
+      id: UuidAdapter.v7(),
+      number: this.lastTicketNumber + 1,
+      createdAt: new Date(),
+      done: false,
+    };
+
+    this.tickets.push(ticket);
+
+    return ticket;
+  }
+
+  public drawTicket(desk: string) {
+    const ticket = this.tickets.find((ticket) => !ticket.handleAtDesk);
+
+    if (!ticket)
+      return { status: "error", message: "There are not pending tickets" };
+
+    ticket.handleAtDesk = desk;
+    ticket.handleAt = new Date();
+    this.workingOnTickets.unshift({ ...ticket });
+
+    // TODO: WS
+    return { status: "ok", ticket };
+  }
+
+  public onFinishedTicket(id: string) {
+    const ticket = this.tickets.find((ticket) => ticket.id === id);
+    if (!ticket) return { status: "error", message: "Ticket not found" };
+
+    this.tickets.map((ticket) => {
+      if (ticket.id === id) ticket.done = true;
+
+      return ticket;
+    });
+
+    return { status: "ok" };
+  }
 }
--- src/presentation/tickets/controller.ts
+++ src/presentation/tickets/controller.ts
@@ -1,29 +1,40 @@
 import { Request, Response } from "express";
+import { TicketService } from "../services";
 
 export class TicketsController {
   // DI - WsService
-  contructor() {}
+  constructor(private readonly ticketService = new TicketService()) {}
 
   public getTickets = async (req: Request, res: Response) => {
-    res.json("getTickets");
+    res.json(this.ticketService.tickets);
   };
 
   public getLastTicketNumber = async (req: Request, res: Response) => {
-    res.json("getLastTicketNumber");
+    res.json(this.ticketService.lastTicketNumber);
   };
   public pendingTickets = async (req: Request, res: Response) => {
-    res.json("pendingTickets");
+    res.json(this.ticketService.pendingTickets);
   };
   public createTicket = async (req: Request, res: Response) => {
-    res.json("createTicket");
+    res.status(201).json(this.ticketService.createTicket());
   };
   public drawTicket = async (req: Request, res: Response) => {
-    res.json("drawTicket");
+    const { desk } = req.params;
+    if (!desk || Array.isArray(desk)) {
+      return res.status(400).json({ error: "Desk parameter is required" });
+    }
+    res.json(this.ticketService.drawTicket(desk));
   };
   public ticketFinished = async (req: Request, res: Response) => {
-    res.json("ticketFinished");
+    const { ticketId } = req.params;
+
+    if (!ticketId || Array.isArray(ticketId)) {
+      return res.status(400).json({ error: "ticketId parameter is required" });
+    }
+
+    res.json(this.ticketService.onFinishedTicket(ticketId));
   };
   public workingOn = async (req: Request, res: Response) => {
-    res.json("workingOn");
+    res.json(this.ticketService.lastWorkingOnTickets);
   };
 }
